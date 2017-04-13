f925a4816c74225c9fffa5056b2218c09fdd9a7f
Joshua <joshua@JW-dev.local>
2017-04-13 15:49:23 -0700

    added edit route and edit frontend

--- routes/snacks.js
+++ routes/snacks.js
@@ -4,11 +4,28 @@ const router = express.Router();
 
 const db = require('../db');
 
-/* GET users listing. */
+// Get all snacks
 router.get('/', (req, res) => {
   db('snacks').then((snacks) => {
     res.render('snacks/index', { snacks });
   });
+});
+
+// Get a single snack
+router.get('/:id', (req, res) => {
+  const id = req.params.id;
+  db('snacks').where({ id }).first()
+  .then((snack) => {
+    res.render('snacks/show-details', { snack });
+  });
+});
+
+router.get('/:id/edit', (req, res) => {
+  const id = req.params.id;
+  db('snacks').where({ id }).first()
+    .then((snack) => {
+      res.render('snacks/edit', { snack });
+    });
 });
 
 module.exports = router;
