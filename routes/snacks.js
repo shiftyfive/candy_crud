de15579e4cdd36bd2d0bd650563edbde133cab7f
Joshua <joshua@JW-dev.local>
2017-04-13 13:07:44 -0700

    added navbar partial to all pages

--- routes/snacks.js
+++ routes/snacks.js
@@ -2,9 +2,13 @@ const express = require('express');
 
 const router = express.Router();
 
+const db = require('../db');
+
 /* GET users listing. */
 router.get('/', (req, res) => {
-  res.send('respond with a resource');
+  db('snacks').then((snacks) => {
+    res.render('snacks/index', { snacks });
+  });
 });
 
 module.exports = router;
