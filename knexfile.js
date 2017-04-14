4ecfb6eb03d4b0c92350da6974a10574658c7e0f
Joshua <joshua@JW-dev.local>
2017-04-14 16:06:43 -0700

    modified knex config file

--- knexfile.js
+++ knexfile.js
@@ -3,14 +3,14 @@ const path = require('path');
 module.exports = {
   production: {
     client: 'pg',
-    connection: process.env.DATBASE_URL,
+    connection: process.env.DATABASE_URL,
     migrations: {
       directory: path.join(__dirname, 'db', 'migrations'),
     },
     seeds: {
       directory: path.join(__dirname, 'db', 'seeds'),
+    },
   },
-},
   development: {
     client: 'pg',
     connection: 'postgres://localhost/snack_crud',
