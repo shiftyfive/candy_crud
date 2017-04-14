c4d89f1465555b5c0fa44db068eedac2fe83061f
Joshua <joshua@JW-dev.local>
2017-04-14 15:26:00 -0700

    first deployment attempt

--- app.js
+++ app.js
@@ -18,7 +18,6 @@ app.set('view engine', 'hbs');
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(methodOverride('_method'));
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
--- routes/snacks.js
+++ routes/snacks.js
@@ -11,6 +11,12 @@ router.get('/', (req, res) => {
   });
 });
 
+// go to new snack form
+router.get('/new', (req, res) => {
+  res.render('snacks/new');
+});
+
+
 // Get a single snack
 router.get('/:id', (req, res) => {
   const id = req.params.id;
@@ -20,14 +26,46 @@ router.get('/:id', (req, res) => {
   });
 });
 
+// send user to edit snack view
 router.get('/:id/edit', (req, res) => {
   const id = req.params.id;
-  db('snacks').where({ id }).first()
-    .then((snack) => {
-      res.render('snacks/edit', { snack });
-    });
+  db('snacks').select('*').where({ id }).first()
+  .then((snack) => {
+    res.render('snacks/edit', { snack });
+  });
 });
 
+router.post('/', (req, res) => {
+  const snack = {
+    snack_picture_url: req.body.snack_picture_url,
+    snack_name: req.body.snack_name,
+    price: req.body.price,
+    rating: req.body.rating,
+  };
+  db('snacks').insert(snack, '*')
+  .then((newSnack) => {
+    const id = newSnack[0].id;
+    res.redirect(`/snacks/${id}`);
+  });
+});
+
+// update snack and redirect user to the updated single snack name
+router.put('/:id', (req, res) => {
+  const id = req.params.id;
+  const snack = {
+    snack_picture_url: req.body.snack_picture_url,
+    snack_name: req.body.snack_name,
+    price: req.body.price,
+    rating: req.body.rating,
+  };
+  db('snacks').update(snack, '*').where({ id }).then((updatedSnack) => {
+    const updatedId = updatedSnack[0].id;
+    res.redirect(`/snacks/${updatedId}`);
+  });
+});
+
+
+// delete a snack
 router.delete('/:id', (req, res) => {
   const id = req.params.id;
   db('snacks').del().where({ id })
@@ -35,5 +73,6 @@ router.delete('/:id', (req, res) => {
     res.redirect('/snacks');
   });
 });
+
 
 module.exports = router;
